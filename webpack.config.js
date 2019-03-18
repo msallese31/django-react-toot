73997f59231cef43d32cdad1d9b1fd76fe6922cb
msallese31 <msallese31@gmail.com>
2019-03-18 14:28:40 -0400

    video 2 completed; React + Django Rest functional

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// "Allows us to use babel to transpile our code"
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
